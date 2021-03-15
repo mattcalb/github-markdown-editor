d0f89a893cad2c396bd467fefb57f568297ab931
mattcalb <mattcalb2002@gmail.com>
2021-03-14 22:48:35 -0300

    formatted the code

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,42 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from "react-markdown";
 
-import gfm from 'remark-gfm'
+import gfm from "remark-gfm";
 
-import gitHubMark from '../../assets/images/GitHub-Mark-32px.png';
+import gitHubMark from "../../assets/images/GitHub-Mark-32px.png";
 
-import './style.css';
+import "./style.css";
 
 function MainPage() {
+  const [input, setInput] = useState(
+    JSON.parse(localStorage.getItem("input")) || ""
+  );
 
-    const [input, setInput] = useState(JSON.parse(localStorage.getItem('input')) || '');
+  function handleChangeInput(e) {
+    setInput(e.target.value);
 
-    function handleChangeInput(e) {
-        setInput(e.target.value);
+    saveInputToLocalStorage(e.target.value);
+  }
 
-        saveInputToLocalStorage(e.target.value);
-    }
+  function saveInputToLocalStorage(input) {
+    localStorage.setItem("input", JSON.stringify(input));
+  }
 
-    function saveInputToLocalStorage(input) {
-        localStorage.setItem('input', JSON.stringify(input));
-    }
-
-    return (
-        <div>
-            <header>
-                <h1>GitHub Markdown Editor</h1>
-                <a className="link" href="https://guides.github.com/features/mastering-markdown/">Learn GitHub Markdown</a>
-            </header>
-            <div className="editor-container">
-                <textarea className="editor" placeholder="Some markdown here..." value={input} onChange={e => handleChangeInput(e)}></textarea>
-                <ReactMarkdown plugins={[gfm]} children={input} className="preview" />
-            </div>
-            <footer>
-                <a href="https://github.com/mattcalb"><img src={gitHubMark} alt="GitHub Mark"/></a>
-            </footer>
-        </div>
-    )
-};
+  return (
+    <div>
+      <header>
+        <h1>GitHub Markdown Editor</h1>
+        <a
+          className="link"
+          href="https://guides.github.com/features/mastering-markdown/"
+        >
+          Learn GitHub Markdown
+        </a>
+      </header>
+    
+      <div className="editor-container">
+        <textarea
+          className="editor"
+          placeholder="Some markdown here..."
+          value={input}
+          onChange={(e) => handleChangeInput(e)}
+        ></textarea>
+        <ReactMarkdown plugins={[gfm]} children={input} className="preview" />
+      </div>
+      
+      <footer>
+        <a href="https://github.com/mattcalb">
+          <img src={gitHubMark} alt="GitHub Mark" />
+        </a>
+      </footer>
+    </div>
+  );
+}
 
 export default MainPage;
